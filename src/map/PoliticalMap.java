28c6abe9ee31019a948c86bfd89f04d417f48e15
hcps-mcenhimlr <hcps-mcenhimlr@180-18MCENHIMLR.hcps.internal>
2016-04-28 15:09:07 -0400

    The program can read input from a all of the text files; however, there are undoubtedly better ways to read in the data. There is a theoretical outline for drawing the polygons; however, the program has decided it doesn't want to follow that shape. That was temporarily replaced by dots, to ensure the data can work properly. Unfortunately, the map is drawn backwards. HI also doesn't work.

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -23,28 +23,50 @@ import java.util.*;
 public class PoliticalMap {
     
     static int numberTwo;
+    static int holder = 0;
     
     public static void main(String[] args) throws Exception{
         
         ////////////////////////////////////////////////////////////////////////
         
-        /*File file = new File("AL.txt");//creates the file.
-        Countries country = new Countries();//Creates an instance of the countries class. Potentially useless*/
-        System.setIn(new FileInputStream("C:\\Users\\hcps-mcenhimlr\\Documents\\(6) Programming\\Java\\NetBeans\\GitHub\\Purple America\\PurpleAmerica\\src\\data\\AK.txt"));
-        
-        //Scanner scan = new Scanner(file);//Creates a scanner to read the file.
+        /*String text = "C:\\Users\\hcps-mcenhimlr\\Documents\\(6) Programming\\Java\\NetBeans\\GitHub\\Purple America\\PurpleAmerica\\src\\map\\Abbreviations.txt";
 
+        System.setIn(new FileInputStream(text));
+        for(int i=0; i<=holder; i++){
+            System.out.println(StdIn.readString());
+            
+        }
+        */
+        String [] abbreviations = {"AK.txt", "AL.txt", "AR.txt", "AZ.txt", "CA.txt", "CO.txt", "CT.txt", "DC.txt", "DE.txt", "FL.txt", "GA.txt", /*"HI.txt",*/ "IA.txt", "ID.txt", "IL.txt", "IN.txt",  "KS.txt", "KY.txt",
+         "LA.txt", "MA.txt", "MD.txt", "ME.txt", "MI.txt", "MN.txt", "MO.txt", "MS.txt", "MT.txt", "NC.txt", "ND.txt", "NE.txt", "NH.txt", "NJ.txt", "NM.txt", "NV.txt", "NY.txt", "OH.txt", "OK.txt",
+                 "OR.txt", "PA.txt", "RI.txt", "SC.txt", "SD.txt", "TN.txt", "TX.txt", "UT.txt", "VA.txt", "VT.txt", "WA.txt", "WI.txt", "WV.txt", "WY.txt"};
+        while(holder < abbreviations.length){
+        
+        
+        String fileName = abbreviations[holder];
+        holder++;
+        System.out.println(fileName);
+        
+        /*String textTwo = "C:\\Users\\hcps-mcenhimlr\\Documents\\(6) Programming\\Java\\NetBeans\\GitHub\\Purple America\\PurpleAmerica\\src\\data\\" + fileName;
+        System.setIn(new FileInputStream(textTwo));
+        */File file = new File("C:\\Users\\hcps-mcenhimlr\\Documents\\(6) Programming\\Java\\NetBeans\\GitHub\\Purple America\\PurpleAmerica\\src\\data\\" + fileName);
+        Scanner scan = new Scanner(file);
+        /*
+        System.out.println("hu" + StdIn.readString());
+        
+        System.out.println(("C:\\Users\\hcps-mcenhimlr\\Documents\\(6) Programming\\Java\\NetBeans\\GitHub\\Purple America\\PurpleAmerica\\src\\data\\AK.txt"));
+        */
         
         //makes it so that you can see the pen.
-        StdDraw.setPenRadius(0.0001);//changes the pen size
+        StdDraw.setPenRadius(0.0005);//changes the pen size
         StdDraw.setPenColor(StdDraw.BLACK);//changes the pen color
         
         //Draws points at the highest and lowest points of the graph
         for(int i=0; i<2; i++){
-            StdDraw.point(Math.abs(StdIn.readDouble()/300), Math.abs(StdIn.readDouble()/300));//scan.nextInt(), scan.nextInt());
+            StdDraw.point(Math.abs(scan.nextDouble()/300), Math.abs(scan.nextDouble()/300));//scan.nextInt(), scan.nextInt());
         }//end for
         
-            int number = StdIn.readInt();//Gets the number of countries that make up the state
+            int number = scan.nextInt();//Gets the number of countries that make up the state
                         
             boolean keepGoing;
         
@@ -56,10 +78,10 @@ public class PoliticalMap {
             String next;
             
             while(bad){
-                if(StdIn.hasNextChar()){
-                    next = StdIn.readString();
+                if(scan.hasNext()){
+                    next = scan.next();
                     
-                    System.out.println("next: " +  next);
+                    //System.out.println("next: " +  next);
                     
                     
                     if((next.charAt(0)<65)){
@@ -83,27 +105,29 @@ public class PoliticalMap {
         
                                
         ////////////////////////////////////////////////////////////////////////
-        
-            System.out.println(numberTwo);
-            
+                    
         if(keepGoing = true){
             double[] x = new double[numberTwo+10];
             double[] y = new double[numberTwo+10];
 
             for(int i=0; i<numberTwo; i++){
-                    
-                    x[i] = Math.abs(StdIn.readDouble()/300);
+                    StdDraw.point(Math.abs(scan.nextDouble()/200), Math.abs(scan.nextDouble()/200));
+                    /*x[i] = Math.abs(StdIn.readDouble()/300);
                     y[i] = Math.abs(StdIn.readDouble()/300);
                 }
 
-                StdDraw.polygon(x, y);
-                
+                StdDraw.polygon(x, y);*/
+            }
                 numberTwo = 0;
+                
+                
+                
             }
             
         
         
         }
         
+    }
     }
 }
